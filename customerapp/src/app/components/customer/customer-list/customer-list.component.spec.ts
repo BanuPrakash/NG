3fb2e50e80f1804e0b2359409c4e4130cfa847b0
Banuprakash <banuprakashc@yahoo.co.in>
2022-11-17 15:05:27 +0530

    day 2

--- customerapp/src/app/components/customer/customer-list/customer-list.component.spec.ts
+++ customerapp/src/app/components/customer/customer-list/customer-list.component.spec.ts
@@ -1,14 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Customer } from 'src/app/shared/model/Customer';
+import { CustomerCardComponent } from '../customer-card/customer-card.component';
 
 import { CustomerListComponent } from './customer-list.component';
 
 describe('CustomerListComponent', () => {
   let component: CustomerListComponent;
   let fixture: ComponentFixture<CustomerListComponent>;
-
+  let customers:Customer[] = [{
+    "id": 1,
+    "firstName": "Rachel",
+    "lastName": "Green ",
+    "gender": "female",
+    "address": "M G Road"
+  },
+  {
+    "id": 4,
+    "firstName": "Monica",
+    "lastName": "Geller",
+    "gender": "female",
+    "address": "M G Road"
+  },
+  {
+    "id": 5,
+    "firstName": "Ross",
+    "lastName": "Geller",
+    "gender": "male",
+    "address": "M G Road"
+  } 
+  ];
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CustomerListComponent ]
+      declarations: [ CustomerListComponent, CustomerCardComponent ],
+      imports: [FormsModule]
     })
     .compileComponents();
 
@@ -17,7 +42,19 @@ describe('CustomerListComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create 3 customers', () => {
+    component.customers = component.original = customers; // mock data
+    fixture.detectChanges();
+    let cards = fixture.nativeElement.querySelectorAll(".card");
+    expect(cards.length).toEqual(3);
   });
+
+  it("should delete a customer", () => {
+    component.customers = component.original = customers; // mock data
+    component.deleteCustomerFromList(4);
+    fixture.detectChanges();
+    let cards = fixture.nativeElement.querySelectorAll(".card");
+    expect(cards.length).toEqual(2);
+  });
+
 });
