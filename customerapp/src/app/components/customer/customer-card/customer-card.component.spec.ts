a6a916b3e7e822913b0277bd2c0555ade06828f6
Banuprakash <banuprakashc@yahoo.co.in>
2022-11-17 15:21:35 +0530

    day 2

--- customerapp/src/app/components/customer/customer-card/customer-card.component.spec.ts
+++ customerapp/src/app/components/customer/customer-card/customer-card.component.spec.ts
@@ -20,4 +20,48 @@ describe('CustomerCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should render customer card", () => {
+    component.customers = [{
+      "id": 1,
+      "firstName": "Rachel",
+      "lastName": "Green ",
+      "gender": "female",
+      "address": "M G Road"
+    },
+    {
+      "id": 2,
+      "firstName": "Chandler",
+      "lastName": "Bing",
+      "gender": "male",
+      "address": "M G Road"
+    }];
+    fixture.detectChanges();
+    let cards = fixture.nativeElement.querySelectorAll('.card');
+    expect(cards.length).toEqual(2);
+  });
+
+  it("testing @Output by mocking", () => {
+    component.customers = [{
+      "id": 1,
+      "firstName": "Rachel",
+      "lastName": "Green ",
+      "gender": "female",
+      "address": "M G Road"
+    },
+    {
+      "id": 2,
+      "firstName": "Chandler",
+      "lastName": "Bing",
+      "gender": "male",
+      "address": "M G Road"
+    }];
+    fixture.detectChanges();
+    // jest.fn()
+    const delEvent = spyOn(component.delEvent, 'emit');
+    let delBtn = fixture.nativeElement.querySelectorAll('.fa-trash');
+    delBtn[0].click();
+    expect(delEvent).toHaveBeenCalled();
+    expect(delEvent).toHaveBeenCalledWith(1);
+  });
 });
